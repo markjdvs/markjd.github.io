be93f459fbe9f73759e366de79f17b1974e14f42
mrkjdvs <mjd814@gmail.com>
2017-05-05 16:53:45 +0100

    github links added & styling

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(() => {
       content: `A moving puzzle game with similar logic to that of Candy Crush. During the build, the first short sprint to a 3 by 3 grid meant that I could see a scaleable solution early on during the project. I began again from scratch as major functions required near complete redefinition.
 
       Calculating the particular geometric patterns required to make these scaleable functions was the biggest issue I encountered. By nesting a loop within a loop I could pass through the index from the first to the second, which allowed these functions to check and remove matching chains of symbols across two axes. The board size now depends on one variable at the beginning of the code, and fully functional regardless of its value.`,
+      github: `https://github.com/markjdvs/project-game`,
       link: `https://secure-dawn-20517.herokuapp.com/`,
       class: 'fern'
 
@@ -18,6 +19,7 @@ $(() => {
       The first problem I came to was normalising the recommendations’ or “twists”’ main ingredient of new entries. I did this using the Tesco API, which makes a request based on the user’s input. The vast data response from the API needed thorough filtering and the necessary attributes of the product passed through to the database using hidden inputs.
 
       A personal favourite of this project, although simple, involved a dynamic creation of the tastes of each cocktail. I employed a binary ratings system in the database that is checked by the front-end, and in turn renders icons on the page to reflect these tastes.`,
+      github: `https://github.com/markjdvs/project-full-stack-app`,
       link: `https://afternoon-earth-16892.herokuapp.com/`,
       class: 'drank'
     },{
@@ -27,6 +29,7 @@ $(() => {
       An enjoyable challenge I found during this build was finding a way of adding users to groups and rendering this live addition on the page. Using resource promises, I filtered through the users based on the user’s input and on a click the specified user was added to the group and rendered inside the “added” section of the group.
 
       Another issue required population of two models in both directions. Referenced models didn’t allow such relationships, as I needed the id of a model’s instance before it was created. I solved this issue using a pre-save lifecycle hook to attach a virtual to the new instance and then populated this using Mongoose’s built-in feature.`,
+      github: `https://github.com/markjdvs/project-3`,
       link: 'https://pnc-app.herokuapp.com/',
       class: 'rite'
     }, {
@@ -36,6 +39,7 @@ $(() => {
       I used custom serializers to attach attributes to “stops” along a user’s trip. I wanted to ensure that the ratings were reflective of people who had visited these places, so the average rating attributes that I attached to these stops were an average across all associated posts. These ratings are then rendered on click of a specific marker in our “WorldBlog”. Another aspect that we wanted to employ was user-driven voting of posts that were deemed truthful and/or useful to users.
 
       By integrating the Acts As Votable gem into our back-end I was able to write custom methods for these upvote and downvote functions. By then serializing this data once again, an overall vote tally was provided for each post to the Angular app out front, lightening the load and ensuring the speed of our app was not diminished.`,
+      github: `https://github.com/markjdvs/project-4-frontend`,
       link: 'https://secure-plateau-23163.herokuapp.com/',
       class: 'guide'
     }
@@ -76,7 +80,15 @@ $(() => {
       $('body').append(`<div><div><div class='projects'></div></div></div>`);
       $.each(projects, (i, project) => {
         setTimeout(() => {
-          $('div.projects').append(`<div class='${project.class}'><h1>${project.appName}</h1><p>${project.content}</p><a href='${project.link}' target='_blank'><img src='images/playIcon.png'></a><span><img class='infoDown' src='images/infoDown.png' data-proj='${project.class}'></span><span><img class='infoUp' src='images/infoUp.png' data-proj='${project.class}'></span></div>`);
+          $('div.projects').append(`
+            <div class='${project.class}'>
+              <h1>${project.appName}</h1>
+              <p>${project.content}</p>
+              <a href='${project.github}' target='_blank'><img src='images/github.png'></a>
+              <a href='${project.link}' target='_blank'><img src='images/playIcon.png'></a>
+              <span><img class='infoDown' src='images/infoDown.png' data-proj='${project.class}'></span>
+              <span><img class='infoUp' src='images/infoUp.png' data-proj='${project.class}'></span>
+            </div>`);
        }, i*100);
       });
       $('div.projects').on('click', 'img.infoDown', toggleInfo);
