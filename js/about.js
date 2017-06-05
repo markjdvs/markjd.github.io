dbfd71a4a445ccc111a69ab0711efd749192d68e
mrkjdvs <mjd814@gmail.com>
2017-06-05 16:39:00 +0100

    messing with styling

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
 const about = {
   content1: `Hi. <br> I'm Mark, great to meet you.`,
-  content2: `I'm a web developer working in London. <br><br> Languages are my game and coding is another that I've had a pop at. <br><br> Hope you enjoy my work and feel free to contact me via your chosen method of commmunication.`
+  content2: `I'm a web developer working in London. <br><br> Languages are my game and coding is another that I've had a pop at. <br><br> Hope you enjoy my work.`
 };
