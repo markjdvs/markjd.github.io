17c7ee6eaefc6878155974836827e89c8b11f5be
mrkjdvs <mjd814@gmail.com>
2017-06-20 12:04:29 +0100

    added twitter and medium links

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 const about = {
   content1: `Hi I'm Mark, great to meet you!<br><br>`,
-  content2: `I'm a web developer working in London. <br><br> I love languages and coding is one of them.<br><br> Feel free to contact me via the methods below or you can download my CV <a href="./images/Mark_Jonathan_Davis.pdf" target="_blank">here</a>.`,
+  content2: `I'm a web developer working in London. <br><br> I love languages and coding is one of them.<br><br> Feel free to contact me via the methods below, follow me on <a href="https://twitter.com/markjdvs">twitter</a> or <a href="https://medium.com/@markjdvs">medium</a> or you can download my CV <a href="./images/Mark_Jonathan_Davis.pdf" target="_blank">here</a>.`,
   github: 'https://github.com/markjdvs',
   linkedIn: 'https://www.linkedin.com/in/markjdvs'
 };
