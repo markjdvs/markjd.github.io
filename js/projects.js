a8f7d729e137ecfab967c11bd060f95c136d73c9
mrkjdvs <mjd814@gmail.com>
2017-06-22 15:48:22 +0100

    Updated github links to repos

--- js/projects.js
+++ js/projects.js
@@ -2,26 +2,26 @@ const projects = [
   {
     appName: 'Between Many Ferns',
     content: `A moving puzzle game similar to Candy Crush with the ability to vary the size of the board. The objective is to match chains of tiles together in order to gain points, and new tiles cascade down to refill the board.`,
-    github: `https://github.com/markjdvs/project-game`,
+    github: `https://github.com/markjdvs/between-many-ferns`,
     link: `https://secure-dawn-20517.herokuapp.com/`,
     class: 'fern'
 
   }, {
     appName: 'GuideBlog',
     content: `A voluntarily pair-coded project built with a Ruby on Rails back-end API, with an Angular front-end and JWT authentication. This mobile-first app is driven by users’ blog posts from visiting areas around the world to create clusters of interest across the globe that new users can tap into in order to plan future trips.`,
-    github: `https://github.com/markjdvs/project-4-frontend`,
+    github: `https://github.com/markjdvs/guideblog-fe`,
     link: 'https://secure-plateau-23163.herokuapp.com/',
     class: 'guide'
   }, {
     appName: 'Last.drank',
     content: `A fun, user-driven app for finding recommendations to preferred cocktails. Built with an Express back-end, using session-type authentication and OAuth Github & Facebook authorisations, and a jQuery front-end, this was very much an app after my own heart. It is an app that helps you to try something new and different, but still to your taste.`,
-    github: `https://github.com/markjdvs/project-full-stack-app`,
-    link: `https://afternoon-earth-16892.herokuapp.com/`,
+    github: `https://github.com/markjdvs/last-drank`,
+    link: `https://last-drank.herokuapp.com/`,
     class: 'drank'
   }, {
     appName: 'Ritemove',
     content: `A group project that provides a platform for finding rental properties as a group. An Express app with an Angular front-end which allows the user to search for properties and compile favourites into a group profile page. Photos uploaded by members of the group can then be commented on and the property rated.`,
-    github: `https://github.com/markjdvs/project-3`,
+    github: `https://github.com/markjdvs/ritemove`,
     link: 'https://pnc-app.herokuapp.com/',
     class: 'rite'
   }
